80ca2e7fb853e280cac2ea4649bd0bc0ba9e2ed0
c.w <c.werberger@stud.uni-hannover.de>
2021-11-06 16:00:25 +0100

    bugfix

--- rtl/pulp_soc/soc_interconnect_wrap.sv
+++ rtl/pulp_soc/soc_interconnect_wrap.sv
@@ -120,7 +120,7 @@ module soc_interconnect_wrap
        '{ idx: 0, start_addr: `SOC_MEM_MAP_AXI_PLUG_START_ADDR,    end_addr: `SOC_MEM_MAP_AXI_PLUG_END_ADDR},
        '{ idx: 1, start_addr: `SOC_MEM_MAP_PERIPHERALS_START_ADDR, end_addr: `SOC_MEM_MAP_PERIPHERALS_END_ADDR},
        '{ idx: 2, start_addr: `SOC_MEM_MAP_GF_MULT_START_ADDR,     end_addr: `SOC_MEM_MAP_GF_MULT_END_ADDR}, // add new Adress Rule for the new gf IP
-       '{ idx: 2, start_addr: `SOC_MEM_MAP_VITERBI_START_ADDR,     end_addr: `SOC_MEM_MAP_VITERBI_END_ADDR}}; // add new Adress Rule for Viterbi
+       '{ idx: 3, start_addr: `SOC_MEM_MAP_VITERBI_START_ADDR,     end_addr: `SOC_MEM_MAP_VITERBI_END_ADDR}}; // add new Adress Rule for Viterbi
     //For legacy reasons, the fc_data port can alias the address prefix 0x000 to 0x1c0. E.g. an access to 0x00001234 is
     //mapped to 0x1c001234. The following lines perform this remapping.
     XBAR_TCDM_BUS tcdm_fc_data_addr_remapped();
